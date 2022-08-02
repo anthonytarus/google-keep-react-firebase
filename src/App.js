372cbc2db48b4c900bc7706a84ffdf325b663588
unknown <antonykibet03@gmail.com>
2022-08-02 22:37:35 +0300

    popup for editing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Header from "./components/Header";
-import {BsPinAngle} from'react-icons/bs'
-import {AiOutlinePlus} from 'react-icons/ai'
+import { BsPinAngle } from "react-icons/bs";
+import {BiPencil} from 'react-icons/bi'
+import { AiOutlinePlus } from "react-icons/ai";
 import { useState } from "react";
+import Edit from "./components/Edit";
+
 function App() {
   const [extend, setExtend] = useState(false);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [notes, setNotes] = useState([]);
-  
 
   const addNote = () => {
     const newNote = { title, content };
@@ -26,7 +28,7 @@ function App() {
           {extend && (
             <input
               type="text"
-              className=""
+              className="p-1 outline-none text-xl"
               placeholder="Title"
               name="title"
               onChange={(e) => setTitle(e.target.value)}
@@ -34,7 +36,7 @@ function App() {
           )}
 
           <textarea
-            className=""
+            className="outline-none text-xl"
             placeholder="Take a note..."
             onChange={(e) => setContent(e.target.value)}
           ></textarea>
@@ -43,35 +45,35 @@ function App() {
               className="bg-addBtn rounded-full w-10 h-10 cursor-pointer flex items-center justify-center text-4xl text-white"
               onClick={addNote}
             >
-              <AiOutlinePlus/>
+              <AiOutlinePlus />
             </p>
           </div>
         </div>
       </section>
 
       <section className="p-7 md:p-16 grid md:grid-cols-2 xl:grid-cols-4 gap-4">
-        
         <div className="bg-card rounded-lg h-28 p-4 hover:shadow-lg border relative">
           <h2 className="font-bold capitalize text-gray-700">Default Note</h2>
-          <p className="text-gray-600">
-            Had fun building this ❤❤       
-          </p>
-          <div className="absolute top-5 right-5 text-xl">
-          <BsPinAngle />
-        </div>
-        
-        
+          <p className="text-gray-600">Had fun building this ❤❤</p>
+          <div className="absolute top-3 right-4">
+            <BsPinAngle size={20}/>
+          </div>
         </div>
 
         {notes.map((note) => (
-          <div className="bg-card rounded-lg  p-4 hover:shadow-lg border relative">
-            <h2 className="font-bold capitalize text-gray-700">{note.title}</h2>
+          <div className="bg-card rounded-lg  p-4 hover:shadow-lg border">
+            <div className="flex">
+            <h2 className="font-bold capitalize text-gray-700 w-[100%]">{note.title}</h2>
+            <p className="w-[6%] cursor-pointer"><BiPencil size={20}/></p>
+            </div>
+            
             <p className="text-gray-600">{note.content}</p>
-            <p className="absolute top-0 right-0">edit</p>
-            <p className="absolute top-0 right-0">edit</p>
+           
+            
           </div>
         ))}
       </section>
+      <Edit/>
     </div>
   );
 }
